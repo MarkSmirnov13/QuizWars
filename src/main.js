3177fc8e23fb481a9ddf77c87c11825265f26259
MarkSmirnov13 <marksmirnov13@gmail.com>
2019-09-23 01:02:03 +0300

    Добавлена возможность добавления задачки

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 
 import './database/queries/database'
-import {addNewUser, getRandomTask, findTaskById} from './database/queries/user'
+import {addNewUser} from './database/queries/user'
+import {getRandomTask, findTaskById, addNewTaskToDb} from './database/queries/task'
 import {provideKeyboard, resolveAnswer} from './helpers'
 
 const TelegramBot = require('node-telegram-bot-api')
@@ -11,7 +12,6 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
 /**
  * Добавляет id нового пользователя в базу данных
  */
-
 bot.onText(/\/start/, ({chat: {id}}) => {
     addNewUser(id)
       .then(() => bot.sendMessage(id, 'Welcome to the club buddy!'))
@@ -20,7 +20,6 @@ bot.onText(/\/start/, ({chat: {id}}) => {
 /**
  * Предлагает пользователю рандомную задачу
  */
-
 bot.onText(/\/random/, ({chat: {id}}) => {
     getRandomTask()
       .then(task => bot.sendMessage(
@@ -51,3 +50,38 @@ bot.on('callback_query', ({message: {chat, message_id}, data}) => {
       }
     ))
 })
+
+/**
+ * Добавляем новую задачу в базу
+ */
+const opts = {
+    reply_markup: JSON.stringify(
+        {
+            force_reply: true
+        }
+)};
+
+const text = [
+    'Отлично, напиши условие задачи!',
+    'А теперь напиши первый вариант ответа',
+    'Теперь напиши второй вариант ответа',
+    'Теперь напиши третий вариант ответа',
+    'Теперь напиши четвертый вариант ответа',
+    'Теперь напиши НОМЕР правильного варианта ответа',
+    'Теперь напиши сколько очкой будет давать задача',
+]
+
+const addNewTask = (id, content = [], i = 0) => bot.sendMessage(id, text[i], opts)
+    .then(({chat, message_id}) => {
+        bot.onReplyToMessage(chat.id, message_id, message => {
+            content.push(message.text)
+            if (i < 6)
+                addNewTask(id, content, ++i)
+            else {
+                addNewTaskToDb(content)
+                bot.sendMessage(id, 'Молодец! Задача будет добавлена!')
+            }
+        });
+    })
+
+bot.onText(/\/add_new_task/, ({chat: {id}}) => addNewTask(id))
